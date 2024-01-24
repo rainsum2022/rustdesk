1630e99211fc37dcaebf1bd3e85507bb3f99fc0d
Rain Sun <rainss.cn@gmail.com>
2024-01-24 15:06:51 +0800

    Update config.rs
    private server

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -94,8 +94,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["hbbs.rains.autos"];
+pub const PUBLIC_RS_PUB_KEY: &str = "RBpKEUU0Sjr0+z41zqbaIPGF96HMVgdRzHyj4GoQE90=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
